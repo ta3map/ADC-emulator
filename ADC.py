55a3a0833441c5c1f1e426c8a571e7eefe5f3607
Azat Gainutdinov <50362765+ta3map@users.noreply.github.com>
2021-04-05 19:58:17 +0300

    new resampling
    sample size error and too high tone on high Fs error deleted using resample_poly

--- ADC.py
+++ ADC.py
@@ -1,34 +1,54 @@
 import pandas as pd
 import numpy as np
+from scipy.signal import resample_poly
+from scipy.signal import resample
 
-def hello():
-    print('ADC is imported')
-    return None
+def rec(v, samples, quant_levels, target_fs):
+    folder = '/content/ADC_simulator/'
+    #folder = r'C:\Users\Azat\Documents\Digital Signal Processing\Lab. 2\ADC_simulator_2021/'
+    path = folder + 'data/data'+ str(v) +'_v2'
 
-def rec(v, samples, quant_levels, samplerate):
-    path = '/content/ADC_simulator/data/data'+ str(v) +'_v2'
-
+    def is_int(x):
+        try:
+             int(x)
+             return True
+        except:
+             return False
+         
+    def quantization(signal, quant_levels):
+        quanted = np.zeros(np.size(signal));
+        for i in range(len(quant_levels)):
+            quanted[signal>quant_levels[i]] = quant_levels[i]; 
+        return quanted
+    
+    def change_size(signal, samples):
+        size_dif = len(signal) - samples
+        if size_dif<0:
+            std_y = np.std(signal)
+            mean_y = np.mean(signal)
+            a = std_y/2
+            nothing = a*np.random.rand(np.abs(size_dif))+mean_y-a/2
+            output = np.append(signal, quantization(nothing, quant_levels))
+        else:                
+            output = quanted[0:samples]
+        return output
+        
     y = pd.read_csv(path,  header=None).to_numpy().flatten()
-    Fs, y = int(y[0]), y[1:]
+    original_fs, signal = int(y[0]), y[1:]
     
-    difference = int(Fs*(samples/samplerate - (len(y)/Fs)))
+ 
     
-    if difference>=0:
-        std_y = np.std(y)
-        mean_y = np.mean(y)
-        a = std_y/2
-        y = np.append(y, a*np.random.rand(difference,)+mean_y-a/2)
-    else:
-        y = y[0:int(len(y)+difference)]
+    least_common_divisor = np.lcm(original_fs,target_fs)
+    upsample_factor = least_common_divisor/original_fs
+    downsample_factor = least_common_divisor/target_fs    
+
     
-    # ADC :
-    converted_y = np.zeros(np.size(y));
-    
-    i = 0;
-    for level in quant_levels:
-        converted_y[y > level] = level;
-        i += 1
-    
-    rate_step = round(Fs/samplerate);
-    converted_y = converted_y[0:np.size(y):rate_step]
-    return converted_y
+    assert is_int(downsample_factor) and is_int(upsample_factor), f"Unfortunately we cannot sample with the requested sampling frequency: {target_fs}. Please try some other value."
+    #resampled = resample_poly(signal, upsample_factor, downsample_factor)
+    resampled = resample_poly(signal, upsample_factor, 1)
+    resampled = resampled[0:len(resampled):int(downsample_factor)]
+
+    quanted = quantization(resampled, quant_levels)
+    output = change_size(quanted, samples)
+
+    return output
